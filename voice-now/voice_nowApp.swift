67cd29aab4b0cccf6d15cc0f5c119ca0b562e1cd
lakphy <lakphy@outlook.com>
2025-12-26 22:21:52 +0800

    Initial Commit

---
+++ voice-now/voice_nowApp.swift
@@ -0,0 +1,17 @@
+//
+//  voice_nowApp.swift
+//  voice-now
+//
+//  Created by Lakphy on 2025/12/26.
+//
+
+import SwiftUI
+
+@main
+struct voice_nowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
